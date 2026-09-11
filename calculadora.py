e56d9cd762406453286aaa401e04d40b192e031e
Gustavo Gabriel <gustavogabrielbs15@gmail.com>
2026-09-11 20:04:21 -0300

    Coloquei subtração e corrigi alguns erros

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,10 @@
 def soma (num1, num2):
-    return
+    soma = num1 + num2 
+    return soma
 
 print("Resultado da soma",soma(16,9))
 
 def sub (num1, num2):
-    return
-
-print("resultado da Subtração", sub(89,33))
+    sub= num1 - num2
+    return sub
+print("Resultado da subtração", sub(10,5))
